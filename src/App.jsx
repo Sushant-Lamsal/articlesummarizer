53200757a727fcbdaf2d8e88d3693520548d7a39
Sushant Lamsal <sushantlamsal31@gmail.com>
2023-09-02 12:31:22 +0545

    completed : hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+import './App.css';
+
 const App = () => {
 return (
-    <div>App</div>
+    <main>
+        <div className="main">
+            <div className="gradient"/>
+        </div>
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
 )
 }
 
